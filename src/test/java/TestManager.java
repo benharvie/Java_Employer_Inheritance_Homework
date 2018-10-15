99ceeda8b1c53a313528375f11bfefb881d532df
Ben <benharvie94@gmail.com>
2018-10-15 20:14:42 +0100

    ManagerTest all passing

--- src/test/java/TestManager.java
+++ src/test/java/TestManager.java
@@ -17,4 +17,30 @@ public class TestManager {
     public void hasName(){
         assertEquals("Ben", manager.getName());
     }
+
+    @Test
+    public void hasNiNumber(){
+        assertEquals(12345, manager.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(1450.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("I.T.", manager.getDeptName());
+    }
+
+    @Test
+    public void raisesSalary(){
+        manager.raiseSalary(5.00);
+        assertEquals(1455.00, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void paysBonus(){
+        assertEquals( 14.55, manager.payBonus(), 0.1); // returns 14.5? Need to sort Delta value
+    }
 }
