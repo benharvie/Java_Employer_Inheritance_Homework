ef8c98349e821ce68bfa8ab315f7334ed0dee556
Ben <benharvie94@gmail.com>
2018-10-15 19:58:46 +0100

    Employee class completed

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,4 +1,34 @@
 package Staff;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private int niNumber;
+    private double salary;
+
+    public Employee(String name, int niNumber, double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getNiNumber(){
+        return niNumber;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void raiseSalary(double raiseAmount){
+        salary += raiseAmount;
+    }
+
+    public double payBonus(){
+        return (0.01 * salary);
+    }
 }
